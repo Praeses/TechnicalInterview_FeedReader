6162045020b8f18493a654127e3f0eb9c243f1b8
Billy Davis <billy.davis@gmail.com>
2014-10-07 21:21:29 -0500

    Adding my changes

--- src/FeedReader/App_Start/IdentityConfig.cs
+++ src/FeedReader/App_Start/IdentityConfig.cs
@@ -25,6 +25,7 @@ namespace FeedReader
                 AllowOnlyAlphanumericUserNames = false,
                 RequireUniqueEmail = true
             };
+
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
@@ -34,17 +35,20 @@ namespace FeedReader
                 RequireLowercase = true,
                 RequireUppercase = true,
             };
+            
             // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
             // You can write your own provider and plug in here.
             manager.RegisterTwoFactorProvider("PhoneCode", new PhoneNumberTokenProvider<ApplicationUser>
             {
                 MessageFormat = "Your security code is: {0}"
             });
+
             manager.RegisterTwoFactorProvider("EmailCode", new EmailTokenProvider<ApplicationUser>
             {
                 Subject = "Security Code",
                 BodyFormat = "Your security code is: {0}"
             });
+
             manager.EmailService = new EmailService();
             manager.SmsService = new SmsService();
             var dataProtectionProvider = options.DataProtectionProvider;
