519d03c04da1d1e3eb5d5bc3dcce0daf840fd686
MaxDeBenedetti <modsme@gmail.com>
2014-10-09 15:35:43 -0500

    Fixed the log in problem

---
+++ src/FeedReader/Controllers/SingleFeedController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using Terradue.ServiceModel.Syndication;
+
+
+namespace FeedReader.Controllers
+{
+    public class SingleFeedController : Controller
+    {
+        // GET: SingleFeed
+        public ActionResult Index()
+        {
+            string url = "http://www.siliconera.com/feed/";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            foreach (SyndicationItem item in feed.Items)
+            {
+                
+            }
+            return View();
+        }
+    }
+}
---
+++ src/FeedReader/Models/FeedItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace FeedReader.Models
+{
+    public class FeedItem
+    {
+        public string title { get; set; }
+        public string address { get; set; }
+        public int siteID { get; set; }
+    }
+
+    public class FeedItemsDBContext : DbContext
+    {
+        public DbSet<FeedItem> FeedItems { get; set; }
+    }
+}
