5556964b94d3e96ddc4faf72a947ea938eb93669
wallaceg09 <wallaceg09@yahoo.com>
2014-12-21 17:49:53 -0600

    Add filter capabilities to user subscriptions Modify home page view Add create control for UserSubscriptions.rssFeedName

--- src/FeedReader/Controllers/HomeController.cs
+++ src/FeedReader/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using FeedReader.Models;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
 
 namespace FeedReader.Controllers
 {
     public class HomeController : Controller
     {
+        private UserSubscriptionDBContext db = new UserSubscriptionDBContext();
         public ActionResult Index()
         {
-            return View();
+            string username = User.Identity.GetUserName();//FIXME: Not sure if this is safe?
+            var subscriptions = from s in db.UserSubscriptions
+                                select s;
+            subscriptions = subscriptions.Where(n => n.userName.Equals(username));
+            return View(subscriptions);
         }
     }
 }
