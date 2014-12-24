f314ae0244a37a2e4cbf20cfa3b89d5e4a77f8c3
wallaceg09 <wallaceg09@yahoo.com>
2014-12-24 14:49:23 -0600

    Add ability to restrict RSS Feeds by RSS Name

--- src/FeedReader/Controllers/HomeController.cs
+++ src/FeedReader/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
 
 
 namespace FeedReader.Controllers
@@ -9,12 +10,29 @@ namespace FeedReader.Controllers
     public class HomeController : Controller
     {
         private UserSubscriptionDBContext db = new UserSubscriptionDBContext();
-        public ActionResult Index()
+        public ActionResult Index(string rssFeed)
         {
             string username = User.Identity.GetUserName();//FIXME: Not sure if this is safe?
+            var RssLst = new List<string>();
+
+
             var subscriptions = from s in db.UserSubscriptions
                                 select s;
             subscriptions = subscriptions.Where(n => n.userName.Equals(username));
+            
+            var RssQuery = from d in subscriptions
+                           orderby d.rssFeedName
+                           select d.rssFeedName;
+
+            RssLst.AddRange(RssQuery.Distinct());
+
+            ViewBag.rssFeed = new SelectList(RssLst);
+
+            //Search by rss Feed name
+            if(!string.IsNullOrEmpty(rssFeed))
+            {
+                subscriptions = subscriptions.Where(f => f.rssFeedName == rssFeed);
+            }
             return View(subscriptions);
         }
     }
