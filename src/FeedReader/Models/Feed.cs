f373f209a8281031b677cb69ee8da586bbd2f306
Deeze814 <Deeze814@gmail.com>
2015-05-17 15:51:41 -0500

    Last commit removed Feed and FeedArticle classes, adding them back

---
+++ src/FeedReader/Models/Feed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeedReader.Models
+{
+    public class Feed
+    {
+        public int id { get; set; }
+        public String user_id { get; set; }
+        public string channel { get; set; }
+        public String link { get; set;}     
+        public String desc { get; set; }
+
+    }
+}
