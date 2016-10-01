a13532fac3a2da2d25b60fd22afb498edee03a24
Paul <me@me.me>
2016-10-01 12:29:25 -0500

    Adding Migrations configuration and Models.cs for table structures.

---
+++ src/FeedReader/Models/Models.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FeedReader.Models
+{
+    public class RssFeed
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RssFeedId { get; set; }
+
+        [Index(IsUnique=true)]
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(450)]
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public List<RssFeedItem> RssFeedItems { get; set; }
+    }
+
+    public class RssFeedItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("RssFeed")]
+        public int RssFeedId { get; set; }
+
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public string PublishDate { get; set; }
+        public string Content { get; set; }
+
+        public RssFeed RssFeed { get; set; }
+    }
+
+    public class UserRssFeed
+    {
+        [Key]
+        [ForeignKey("RssFeed")]
+        [Column(Order = 1)]
+        public int RssFeedId { get; set; }
+
+        [Key]
+        [ForeignKey("ApplicationUser")]
+        [Column(Order = 2)]
+        public string UserId { get; set; }
+
+        public RssFeed RssFeed { get; set; }       
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+
+    public class FeedReaderContext : ApplicationDbContext
+    {
+        public DbSet<UserRssFeed> UserRssFeeds { get; set; }
+        public DbSet<RssFeed> RssFeeds { get; set; }
+        public DbSet<RssFeedItem> RssFeedItems { get; set; }
+    }
+}
